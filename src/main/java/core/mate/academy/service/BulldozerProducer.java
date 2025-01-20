d9cb5b4030f72e215090bcc68c9015f4e6ca3e4b
mr-green <sergey.slobodianiuk@gmail.com>
2025-01-20 19:17:40 +0200

    type parameter 'T' is deleted

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -4,7 +4,7 @@ import core.mate.academy.model.Bulldozer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BulldozerProducer<T> implements MachineProducer<Bulldozer> {
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> get() {
         List<Bulldozer> bulldozers = new ArrayList<>();
